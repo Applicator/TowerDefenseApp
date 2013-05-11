1de27cef740020b796b18f47d888a5a72046413b
Matt Johnson <matt.patrick.johnson@gmail.com>
2013-05-11 15:32:46 -0400

    Final commit of Old TD game

---
+++ WindowsFormsApplication1/Path.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefense
+{
+    public class Path
+    {
+        private List<Location> pointsOfInterest;
+
+        public List<Location> PointsOfInterest
+        {
+            get { return pointsOfInterest; }
+            set { pointsOfInterest = value; }
+        }
+    }
+}
